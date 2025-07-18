5a891128674d889cde1ca664cc4840565f39f49b
bamg359 <bamg359@gmail.com>
2025-07-17 20:18:11 -0500

    feat/us001 matrices

---
+++ matriz.py
@@ -0,0 +1,15 @@
+
+edades1 = [[15, 13 ,19], [34, 19, 21]]
+
+print(len(edades1))
+print(edades1[1][1])
+
+
+
+
+edades3 = [65,78,81] 
+
+edades1.append(edades3)
+
+for edad in edades1:
+    print(edad)

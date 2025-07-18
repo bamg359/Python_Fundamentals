96aab338cc15dc11787181b259cdfa2e6d67bdda
bamg359 <bamg359@gmail.com>
2025-07-17 19:06:46 -0500

    feat/US001: tema variables

---
+++ variables.py
@@ -0,0 +1,38 @@
+
+""" 
+Esto funciona como 
+un comentario
+multilinea
+ """
+
+id_customer = 1  #esto es un dato de tipo numérico
+name_customer = "Pepito"  # String
+score = 4.5 # Decimal
+isActive = True # Booleano
+
+print(type(score))
+
+
+## Concatenacion
+
+print("Nombre usuario", name_customer)
+print(f"Score:{score}")
+
+
+## Input 
+
+#name= input("Ingrese su nombre: ")
+#salary_custumer = int(input("Ingrese su salario: "))
+
+#print(type(salary_custumer))
+
+name = "mARYA"
+print(id(name))
+name = name.lower().capitalize()
+print(id(name))
+print(name)
+
+
+#Unicode
+
+print(chr(64))
